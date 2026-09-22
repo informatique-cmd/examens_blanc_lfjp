2d3c60fa90ed316d6be95caa648abc9f656de052
Informatique-LFJP <informatique@lfjpsaly.org>
2026-09-22 13:30:59 +0000

    Mise à jour des programmations d'examens LFJP (22/09/2026)

--- src/features/home/constants.ts
+++ src/features/home/constants.ts
@@ -1,4 +1,4 @@
-// Fichier généré automatiquement par le CMS LFJP le 22/09/2026 à 12:57:22
+// Fichier généré automatiquement par le CMS LFJP le 22/09/2026 à 13:28:30
 // Ce fichier est déployé automatiquement sur Vercel lors de chaque commit GitHub.
 
 export const HOME_PAGE_CONTENT = {
